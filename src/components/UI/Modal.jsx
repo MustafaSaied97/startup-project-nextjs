0cc1031b131307350fe2251769bc41a951783df4
mustafa saied <mustafa.saied@hmaserv.com>
2024-08-19 10:40:05 +0300

    enhancement in modal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import * as Icons from '@/assets/icons';
 
-const Modal = ({ isOpen, onClose, children }) => {
+const Modal = ({ isOpen, onClose, children, ...modalProps }) => {
   if (!isOpen) return null;
 
   return (
@@ -10,41 +10,46 @@ const Modal = ({ isOpen, onClose, children }) => {
       {/* backdrop */}
       <div onClick={onClose} className='backdrop fixed z-0 h-screen w-screen bg-gray-500/20 dark:bg-gray-900/30'></div>
       {/* modal structure */}
-      <div className='relative z-10 flex max-h-[70vh] w-[90%] max-w-[500px] flex-col overflow-y-auto rounded-lg border-0 bg-[--tr-bg] px-6 py-3  shadow-lg outline-none focus:outline-none sm:w-[70%] md:w-[60%] lg:w-[50%]'>
+      <div
+        className='relative z-10 flex max-h-[70vh] w-[90%] max-w-[500px] flex-col overflow-y-auto rounded-lg border-0 bg-[--tr-bg] px-6 py-3  shadow-lg outline-none focus:outline-none sm:w-[70%] md:w-[60%] lg:w-[50%]'
+        {...modalProps}
+      >
         {children}
       </div>
     </section>
   );
 };
 
-Modal.Header = ({ onClose }) => (
-  <header className='flex w-full items-start justify-end rounded-t border-gray-300'>
-    <button className='h-6 w-6 rounded-md text-center text-white hover:bg-gray-300' onClick={onClose}>
-      <Icons.Close />
-    </button>
-  </header>
-);
+Modal.Header = ({ children, className = 'flex w-full items-start justify-end rounded-t border-gray-300', onClose, ...headerProps }) => {
+  return (
+    <header className={`${className}`} {...headerProps}>
+      {children || (
+        <button className='h-6 w-6 rounded-md text-center text-white hover:bg-gray-300' onClick={onClose}>
+          <Icons.Close />
+        </button>
+      )}
+    </header>
+  );
+};
 
-Modal.Body = ({ children }) => (
-  <main className='relative flex w-full flex-auto flex-col items-center justify-between gap-3 rounded pb-8 pt-2'>{children}</main>
-);
+Modal.Body = ({
+  children,
+  className = 'relative flex w-full flex-auto flex-col items-center justify-between gap-3 rounded pb-8 pt-2',
+  ...bodyProps
+}) => {
+  return (
+    <main className={`${className}`} {...bodyProps}>
+      {children}
+    </main>
+  );
+};
 
-Modal.Footer = ({ onClose }) => (
-  <footer className='flex h-[40px] items-center justify-between gap-4 border-solid'>
-    <button
-      className='h-full w-full rounded-md border border-[var(--primary-clr)] p-2 text-sm font-bold text-[var(--primary-clr)] hover:shadow-lg dark:shadow-gray-900'
-      type='button'
-      onClick={onClose}
-    >
-      {'general.cancel'}
-    </button>
-    <button
-      className='h-full w-full rounded-md border border-[var(--primary-clr)] bg-[var(--primary-clr)] p-2 text-sm font-bold text-white hover:shadow-lg dark:shadow-gray-900'
-      type='button'
-    >
-      {'cancel_order'}
-    </button>
-  </footer>
-);
+Modal.Footer = ({ children, className = 'flex h-[40px] items-center justify-between gap-4 border-solid', ...footerProps }) => {
+  return (
+    <footer className={`${className}`} {...footerProps}>
+      {children}
+    </footer>
+  );
+};
 
 export default Modal;
--- src/components/generic/GenericModal.jsx
+++ src/components/generic/GenericModal.jsx
@@ -12,14 +12,28 @@ export default function GenericModal() {
       <button onClick={() => setIsOpenModal((prev) => !prev)} className='text-red-400'>
         modal
       </button>
-      <Modal isOpen={true} onClose={closeModal}>
+      <Modal isOpen={isOpenModal} onClose={closeModal}>
         <Modal.Header onClose={closeModal} />
         <Modal.Body>
           <h1 className='text-center text-lg font-normal sm:text-xl'>{'general.cancel_order'}</h1>
           <h4 className='text-center text-sm font-normal text-slate-500 sm:text-base'>{'general.cancel_order_hint'}</h4>
           <div className='m-0 h-fit w-full p-0'>body</div>
         </Modal.Body>
-        <Modal.Footer onClose={closeModal} />
+        <Modal.Footer>
+          <button
+            className='h-full w-full rounded-md border border-[var(--primary-clr)] p-2 text-sm font-bold text-[var(--primary-clr)] hover:shadow-lg dark:shadow-gray-900'
+            type='button'
+            onClick={closeModal}
+          >
+            {'general.cancel'}
+          </button>
+          <button
+            className='h-full w-full rounded-md border border-[var(--primary-clr)] bg-[var(--primary-clr)] p-2 text-sm font-bold text-white hover:shadow-lg dark:shadow-gray-900'
+            type='button'
+          >
+            {'cancel_order'}
+          </button>
+        </Modal.Footer>
       </Modal>
     </>
   );
