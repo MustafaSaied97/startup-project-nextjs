c805bc1669c23a69c3b45d5fa0a3cc778e178733
mustafa saied <mustafa.saied@hmaserv.com>
2024-09-22 17:35:53 +0300

    add new component

--- src/components/ui/Popover.tsx
+++ src/components/ui/Popover.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+'use client';
+import React, { useRef } from 'react';
 
 export default function Popover() {
+  const popoverRef = useRef<HTMLDivElement>(null);
+  const handleToggle = (e: React.ToggleEvent<HTMLDivElement>) => {
+    console.log('newState of popover', e.newState);
+  };
+  const handleClickPopoverBtn = (e: React.MouseEvent<HTMLButtonElement>) => popoverRef.current?.showPopover();
   return (
     <>
       <button popoverTarget='mydiv'>Popover</button>
-      <div className=' fixed m-auto rounded-md bg-gray-300 p-6 backdrop:bg-slate-950/10' popover='auto' id='mydiv'>
+      <button onClick={handleClickPopoverBtn}>ClickPopoverBtn</button>
+      <div
+        ref={popoverRef}
+        className=' fixed m-auto rounded-md bg-gray-300 p-6 backdrop:bg-slate-950/10'
+        onToggle={handleToggle}
+        popover='manual'
+        id='mydiv'
+      >
         <h2>Popover</h2>
         <hr />
         <p>A popover is an element that is placed on top of everything else.</p>
