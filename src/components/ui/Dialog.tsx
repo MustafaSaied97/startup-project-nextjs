ac1f3b7913dea3ba7bba93d2497dd9cef0b81aa9
mustafa saied <mustafa.saied@hmaserv.com>
2024-09-23 09:24:07 +0300

    dialong

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -15,17 +15,20 @@ export default function Dialog() {
   const closeDialogOnOutsideClick = (e: React.MouseEvent<HTMLDialogElement>) => {
     // Check if the click happened outside the dialog content
     if (e.target === dialogRef.current) {
-      // dialogRef.current?.close();
+      dialogRef.current?.close();
     }
-    console.table({ 'e.target': e.target, 'dialogRef.current': dialogRef.current });
   };
 
   return (
     <>
       <button onClick={showDialog}>Open Dialog</button>
 
-      <dialog ref={dialogRef} className='dialog-with-animation m-auto bg-gray-300 p-0' onClick={closeDialogOnOutsideClick}>
-        <div className='p-4'>
+      <dialog
+        ref={dialogRef}
+        className='dialog-animation m-auto bg-transparent p-0 backdrop:bg-slate-600/10 backdrop:backdrop-blur-[1px] backdrop:backdrop-filter'
+        onClick={closeDialogOnOutsideClick}
+      >
+        <div className='rounded-md bg-gray-300 p-4'>
           <h1>Hello from dialog</h1>
           <button onClick={closeDialog}>Close</button>
         </div>
