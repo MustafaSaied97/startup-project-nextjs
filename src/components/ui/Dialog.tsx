914128f7b69c28f3591551792de95cdd92e122c7
mustafa saied <mustafa.saied@hmaserv.com>
2024-09-23 09:52:17 +0300

    dialog

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -1,11 +1,13 @@
 'use client';
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 export default function Dialog() {
+  // const [isOpenDialog,setIsOpenDialog]=us;
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   const showDialog = () => {
     dialogRef.current?.showModal();
+    document.documentElement.style.overflow = 'hidden';
   };
 
   const closeDialog = () => {
@@ -18,6 +20,16 @@ export default function Dialog() {
       dialogRef.current?.close();
     }
   };
+  // useEffect(() => {
+  //   // if (!isScrollBlocked) return;
+  //   document.documentElement.style.overflow = 'hidden';
+  //   return () => {
+  //     document.documentElement.style.overflow = '';
+  //   };
+  // }, []);
+  const onCloseDialog = (e: React.ToggleEvent<HTMLDialogElement>) => {
+    console.log('dialog is closed');
+  };
 
   return (
     <>
@@ -27,11 +39,13 @@ export default function Dialog() {
         ref={dialogRef}
         className='dialog-animation m-auto bg-transparent p-0 backdrop:bg-slate-600/10 backdrop:backdrop-blur-[1px] backdrop:backdrop-filter'
         onClick={closeDialogOnOutsideClick}
+        onClose={onCloseDialog}
       >
         <div className='rounded-md bg-gray-300 p-4'>
           <h1>Hello from dialog</h1>
           <button onClick={closeDialog}>Close</button>
         </div>
+        <main></main>
       </dialog>
     </>
   );
